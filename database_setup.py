1ddb718dae0e865a0628a9aa95681e39c2e23963
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-01-09 22:57:29 +0530

    Setting up database using sqlalchemy

---
+++ database_setup.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+# _____________Configuration_______________________________
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+# ______________Class_______________________________________
+class Restaurant(Base):
+    # --------Table info-----------
+    __tablename__ = 'restaurant'
+
+    # --------Mapper---------------
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class MenuItem(Base):
+    # --------Table info----------
+    __tablename__ = 'menu_item'
+
+    # --------Mapper--------------
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+# __________configuration End of File______________________
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
